9216d71ad07df9060e45601e8fdb97dd41b0c7c7
ongaeshi <ongaeshi0621@gmail.com>
2011-06-26 02:30:59 +0900

    Demo app.

--- app.rb
+++ app.rb
@@ -36,6 +36,16 @@ get '/*' do |path|
   haml path.to_sym
 end
 
-get '/hello' do |path|
-  "Hello, World !!"
+get '/hello*' do |path|
+  num = path.to_i
+  num = 1 if num == 0
+
+  hello = 'Hello '
+  hello = 'Hel ' if (num > 10000)
+  hello = 'H ' if (num > 100000)
+  return "The end." if (num > 1000000)
+    
+  str = "hello#{num} -> <a href=\"hello#{num*2}\">#{num*2}</a>"
+  str += "<p>" + "#{hello}" * num + "</p>"
+  str
 end
