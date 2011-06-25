4963af89b3d0cacc990040f510f6c4d149b0187a
ongaeshi <ongaeshi0621@gmail.com>
2011-06-25 14:50:11 +0900

    好きな方法で起動出来るように
    $ ruby app.rb$ rackup config.ru$ shotgun

--- app.rb
+++ app.rb
@@ -12,25 +12,23 @@ require 'haml'
 $LOAD_PATH.unshift 'lib'
 require 'haml/filters/blockcode'
 
-class App < Sinatra::Base
-  set :app_file, __FILE__
-  set :haml, :format => :html5, :escape_html => true
+set :app_file, __FILE__
+set :haml, :format => :html5, :escape_html => true
 
-  helpers do
-    alias h escape_html
-  end
+helpers do
+  alias h escape_html
+end
 
-  get '/' do
-    haml :index
-  end
+get '/' do
+  haml :index
+end
 
-  get '/*.css' do |path|
-    content_type 'text/css'
-    sass path.to_sym, :sass => {:load_paths => [options.views]}
-  end
+get '/*.css' do |path|
+  content_type 'text/css'
+  sass path.to_sym, :sass => {:load_paths => [options.views]}
+end
 
-  get '/*' do |path|
-    pass unless File.exist?(File.join(options.views, "#{path}.haml"))
-    haml path.to_sym
-  end
+get '/*' do |path|
+  pass unless File.exist?(File.join(options.views, "#{path}.haml"))
+  haml path.to_sym
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
 require 'app'
-run App
+run Sinatra::Application
 
